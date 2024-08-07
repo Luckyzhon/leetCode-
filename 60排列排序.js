6c8d1f2b866b3ecd9c5c52d97d91897cb8ffb734
zhongyuying <294089917@qq.com>
2024-08-07 19:01:33 +0800

    修改

---
+++ 60排列排序.js
@@ -0,0 +1,57 @@
+
+/*60:给出集合 [1,2,3,...,n]，其所有元素共有 n! 种排列 ,按大小顺序列出所有排列情况，并一一标记，当 n = 3 时, 所有排列如下：
+"123"
+"132"
+"213"
+"231"
+"312"
+"321"
+给定 n 和 k，返回第 k 个排列
+示例：输入 n =3 , k = 3 ; 输出：213
+      输入：n = 4，k=9 ；输出：2314
+      输入：n=3，k =1；输出：123
+
+*/
+
+const getPermutation = (n, k) => {
+  const used = new Set();
+
+  let groupNum = 1;
+  for (let i = 1; i <= n; i++) {
+    groupNum = groupNum * i;
+  }
+
+  const helper = (temp) => {      // temp是当前已选的数字数组
+    const progress = temp.length; // progress表示当前已选了几个数字
+
+    if (progress == n) {          // 因为是空降到正确的组，选够了n个即可返回
+      return temp.join('');
+    }
+
+    groupNum = groupNum / (n - progress); // 一个分组有多少个
+
+    for (let i = 1; i <= n; i++) {
+      if (used.has(i)) continue;
+
+      if (k > groupNum) { // k大于一组的个数
+        k = k - groupNum; // 更新k，
+        continue;         // 跳过这一组，即跳过当前的数字i
+      }
+      temp.push(i);        // 选择i
+      used.add(i);         // 记录选择
+      return helper(temp); // 进度+1 继续选
+    }
+  };
+
+  return helper([]);
+};
+
+/*61：旋转链表 ： 给你一个链表的头节点 head ，旋转链表，将链表每个节点向右移动 k 个位置。
+ eg： 1 , 2, 3, 4  k=2 ; 返回 3,4,1,2
+*/
+
+var rotateRight = function (head, k) {
+  
+
+};
+
