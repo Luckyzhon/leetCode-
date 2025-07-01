bd9a6fff0bd974c75fb41f46bb10738f4382d2f4
zhongyuying <294089917@qq.com>
2025-07-01 14:41:53 +0800

    修改

--- 66-二进制求和.js
+++ 66-二进制求和.js
@@ -1,21 +1,21 @@
 
 
 /*
- 给你两个二进制字符串 a 和 b ，以二进制字符串的形式返回它们的和
+ 67：给你两个二进制字符串 a 和 b ，以二进制字符串的形式返回它们的和
 */
 
 var addBinary22 = function (a, b) {
   const lena = a.length;
   const lenb = b.length;
-  const le = Math.max(lena,lenb)
+  const le = Math.max(lena, lenb)
   let re = 0;
-  for(let i=le.length-1; i>=0; i--){
-   re[i] = (lena[i] ?? 0) + (lenb[i] ?? 0);
+  for (let i = le.length - 1; i >= 0; i--) {
+    re[i] = (lena[i] ?? 0) + (lenb[i] ?? 0);
   }
-  for(let j=0;j<=re.length-1;j++){
-    if(re[j]%2===0){
-      re[j] ===0;
-      re[j+1]++;
+  for (let j = 0; j <= re.length - 1; j++) {
+    if (re[j] % 2 === 0) {
+      re[j] === 0;
+      re[j + 1]++;
     }
   }
   return re;
@@ -27,15 +27,15 @@ var addBinary22 = function (a, b) {
  * @param {string} b
  * @return {string}
  */
-var addBinary = function(a, b) {
+var addBinary = function (a, b) {
   let ans = "";
   let ca = 0;
-  for(let i = a.length - 1, j = b.length - 1;i >= 0 || j >= 0; i--, j--) {
-      let sum = ca;
-      sum += i >= 0 ? parseInt(a[i]) : 0;
-      sum += j >= 0 ? parseInt(b[j]) : 0;
-      ans += sum % 2;
-      ca = Math.floor(sum / 2);
+  for (let i = a.length - 1, j = b.length - 1; i >= 0 || j >= 0; i--, j--) {
+    let sum = ca;
+    sum += i >= 0 ? parseInt(a[i]) : 0;
+    sum += j >= 0 ? parseInt(b[j]) : 0;
+    ans += sum % 2;
+    ca = Math.floor(sum / 2);//向下取整
   }
   ans += ca == 1 ? ca : "";
   return ans.split('').reverse().join('');
--- 面试手写题/防抖，节流实现.js
+++ 面试手写题/防抖，节流实现.js
@@ -1,19 +1,49 @@
 
 
 /* 防抖 */
-function debounce (func,delay ) {
+function debounce(func, delay) {
   let timer;
-  if(timer) clearTimeout(timer);
-  return function(){
+  if (timer) clearTimeout(timer);
+  return function () {
     let arge = arguments;
     let current = this;
-    timer = setTimeout(()=>{
-      func.apply(current,arge)
+    timer = setTimeout(() => {
+      func.apply(current, arge)
     }, delay)
   }
 }
 
 
+//节流,可以使用时间搓或者延时器实现
+
+function throttle1(func, delay) {
+  let previous = 0;
+  return function () {
+    let currentTime = Date.now();
+    let arge = arguments;
+    let that = this;
+    if (currentTime - previous > delay) {
+      func.apply(that, arge);
+      previous = currentTime;
+    }
+  }
+}
+
+
+function throttle2 (func,delay){
+  let timer;
+  return function(){
+    let arge = arguments;
+    let that = this;
+    if(!timer){
+      timer = setInterval(()=>{
+        func.apply(that,arge);
+        timer = null;
+      },delay)
+
+    }
+  }
+}
 
 
 
